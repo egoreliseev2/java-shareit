fd41f7f10a12c2c12a46386632bf9110058d0839
Egor Eliseev <egoreliseev2@yandex.ru>
2023-02-17 18:10:33 +0300

    checkstyle fix

--- src/main/java/ru/practicum/shareit/booking/service/BookingServiceImpl.java
+++ src/main/java/ru/practicum/shareit/booking/service/BookingServiceImpl.java
@@ -1,6 +1,5 @@
 package ru.practicum.shareit.booking.service;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
